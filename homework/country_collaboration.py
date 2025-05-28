3a02893db27df5d63ec67f3a331047f6dc59ab25
Tomas Carmona Salazar <tcarmona@unal.edu.co>
2025-05-28 14:15:49 -0500

    Commit

--- homework/country_collaboration.py
+++ homework/country_collaboration.py
@@ -1,111 +1,62 @@
 import os
-
-import folium # type: ignore
-import pandas as pd # type: ignore
-
+import matplotlib.pyplot as plt  # type: ignore
+import pandas as pd              # type: ignore
 
 def load_affiliations():
-"Carga el archivo scopus-papers.csv y retorna un dataframe con la
-columna 'Affiliations'"""
-
-dataframe = pd.read_csv(
-(
-"https://raw.githubusercontent.com/jdvelasq/datalabs/"
-"master/datasets/scopus-papers.csv"
-),
-sep=",",
-index_col=None,
-)[["Affiliations"]]
-return dataframe
+    return pd.read_csv(
+        "https://raw.githubusercontent.com/jdvelasq/datalabs/"
+        "master/datasets/scopus-papers.csv",
+        usecols=["Affiliations"]
+    )
 
 def remove_na_rows(affiliations):
-"""Elimina las filas con valores nulos en la columna 'Affiliations'"""
+    return affiliations.dropna(subset=["Affiliations"]).copy()
 
-affiliations = affiliations.copy()
-affiliations = affiliations.dropna(subset=["Affiliations"])
-
-return affiliations
-
-
-def add_countries_column(affiliations):
-"""Transforma la columna 'Affiliations' a una lista de paises."""
-
-affiliations = affiliations.copy()
-affiliations["countries"] = affiliations["Affiliations"].copy()
-affiliations["countries"] = affiliations["countries"].str.split(";")
-affiliations["countries"] = affiliations["countries"].map(
-lambda x: [y.split(",") for y in x]
-)
-affiliations["countries"] = affiliations["countries"].map(
-lambda x: [y[-1].strip() for y in x]
-)
-affiliations["countries"] = affiliations["countries"].map(set)
-affiliations["countries"] = affiliations["countries"].str.join(", ")
-
-return affiliations
-
-
-def clean_countries(affiliations):
-
-affiliations = affiliations.copy()
-affiliations["countries"] = affiliations["countries"].str.replace(
-"United States", "United States of America"
-)
-return affiliations
-
+def create_countries_column(affiliations):
+    aff = affiliations.copy()
+    aff["countries"] = (
+        aff["Affiliations"]
+        .str.split(";")
+        .map(lambda lst: [s.strip().split(",")[-1] for s in lst])
+        .map(set)
+        .map(sorted)
+        .str.join(", ")
+    )
+    return aff
 
 def count_country_frequency(affiliations):
-"""Cuenta la frecuencia de cada país en la columna 'countries'"""
+    freq = (
+        affiliations["countries"]
+        .str.split(", ")
+        .explode()
+        .value_counts()
+    )
+    return freq
 
-countries = affiliations["countries"].copy()
-countries = countries.str.split(", ")
-countries = countries.explode()
-countries = countries.value_counts()
-return countries
+def make_plot(n_countries):
+    # 1) Asegura carpeta
+    os.makedirs("files", exist_ok=True)
 
+    # 2) Prepara datos
+    aff = load_affiliations()
+    aff = remove_na_rows(aff)
+    aff = create_countries_column(aff)
+    freq = count_country_frequency(aff)
 
-def plot_world_map(countries):
-"""Grafica un mapa mundial con la frecuencia de cada país."""
+    # 3) Guarda completo (opcional)
+    freq.to_csv("files/countries.csv")
 
-countries = countries.copy()
-countries = countries.to_frame()
-countries = countries.reset_index()
+    # 4) Filtra top-N y grafica
+    top_n = freq.head(n_countries)
+    plt.figure(figsize=(10, 6))
+    top_n.plot(kind="bar")
+    plt.title(f"Top {n_countries} países por frecuencia")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
 
-m = folium.Map(location=[0, 0], zoom_start=2)
-
-folium.Choropleth(
-geo_data=(
-"https://raw.githubusercontent.com/python-visualization/"
-"folium/master/examples/data/world-countries.json"
-),
-data=countries,
-columns=["countries", "count"],
-key_on="feature.properties.name",
-fill_color="Greens",
-).add_to(m)
-
-m.save("files/map.html")
-
-def make_worldmap():
-"""Función principal"""
-
-if not os.path.exists("files"):
-os.makedirs("files")
-
-affiliations = load_affiliations()
-affiliations = remove_na_rows(affiliations)
-affiliations = add_countries_column(affiliations)
-affiliations = clean_countries(affiliations)
-countries = count_country_frequency(affiliations)
-countries.to_csv("files/countries.csv")
-plot_world_map(countries)
-
-
-print()
-for i, row in affiliations.head().iterrows():
-print(row.Affiliations)
-print()
-
+    # 5) Devuelve el top-N si quieres usarlo luego
+    return top_n
 
 if __name__ == "__main__":
-make_worldmap()
+    top20 = make_plot(n_countries=20)
